a96ab939bd9722c9513f8057a5fda4e64899aae0
S Kh <khusayan_syerikjan@unimedia.co.jp>
2017-03-22 13:29:04 +0800

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import Server from 'socket.io';
+
+const PORT = 8090;
+
+export default function startServer(store) {
+	const io = new Server().attach(PORT);
+	console.log('Server started on:', PORT);
+
+	store.subscribe(() => io.emit('state', store.getState().toJS()));
+
+	io.on('connection', (socket)=>{
+		socket.emit('state', store.getState().toJS())
+		socket.on('action', store.dispatch.bind(store));
+	});
+}
